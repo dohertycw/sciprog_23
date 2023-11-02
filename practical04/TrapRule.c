88b26484e0bf7ebb0864c7fc2d474d5dde9ba289
U-MSI\doher <dohertycw@gmail.com>
2023-11-02 13:52:42 +0000

    Tidying up of TrapRule.c comments

--- practical04/TrapRule.c
+++ practical04/TrapRule.c
@@ -2,7 +2,8 @@
 #include <math.h>
 
 
-//Initialising all necessary functions. Also making our values of tan a global variable, so it can be called anywhere. I've made radang a global variable for ease of printing and so that it can be used in the area function.
+//Initialising all necessary functions. Also making our values of tan a global variable, so it can be called anywhere. I've made radang a
+//global variable for ease of printing and so that it can be used in the area function.
 float degtorad(float degang);
 float area(int N);
 float tanvalues[12], radang[12];
@@ -13,14 +14,16 @@ int main(void) {
 	int i;
 	float degang, integral;
 	i = 0;
-	//Constructing a for-loop from 0-12. Within we go from 0 -> 60 degrees in steps of 5, convert each step to radians using our degtorad function, place the result into the tan function and storing in our tanvalues array. The result is then printed.
+	//Constructing a for-loop from 0-12. Within we go from 0 -> 60 degrees in steps of 5, convert each step to radians using our 
+	//degtorad function, place the result into the tan function and storing in our tanvalues array. The result is then printed.
 	for(i = 0; i<13; i++) {
 		degang = i*5.0;
 		radang[i] = degtorad(degang);
 		tanvalues[i] = tan(radang[i]);
 		printf ("The tan of %f radians is %f \n", radang[i], tanvalues[i]);
 		}
-	//Calling our area function to provide the area under the curve of tan(x) from 0 -> 60 degrees in steps of 5 using the Trapezoidal Rule and printing.
+	//Calling our area function to provide the area under the curve of tan(x) from 0 -> 60 degrees in steps of 5 using the Trapezoidal
+	//Rule and printing.
 	integral = area(i);
 	printf ("The area under tan(x) from 0 to 60 degrees is %f \n", integral);
 	return 0;
